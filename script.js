a78d8c516e0a02850a215f01fd55763a03b5fb15
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-31 12:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+ function InitialData(){
+		 return new Promise((resolve,reject)=>{
+			 setTimeout(()=>{
+				 resolve([1,2,3,4])
+			 },3000)
+		 })
+ }
+InitialData().then((data)=>{
+	 // alert(data);
+	return data.filter(num=>num%2==0);
+	
+}).then((num)=>{
+		// alert(num)
+
+
+		return new Promise((resolve)=>{
+	setTimeout(()=>{
+		 document.getElementById('output').innerText =num
+
+		
+		resolve(num) 
+	},1000) 
+
+	})
+}).then((value)=>{
+	// alert(value)
+	
+	return value.map((data)=>data*2)
+}).then((number)=>{
+	// alert(number)
+	setTimeout(()=>{
+		document.querySelector("#output").innerText=number
+	},3000)
+})
+
+
+
